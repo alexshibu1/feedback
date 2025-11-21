d8fcc8c1e9f8d6001b01c1783ab487be1d11c824
Alex Shibu <alexshibu04@gmail.com>
2025-11-21 17:19:04 -0500

    Add Link to dashboard page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary">Click me</button>
-    </div>
+    <main>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+      <Link href="/dashboard">Dashboard fix</Link>
+    </main>
   );
 }
