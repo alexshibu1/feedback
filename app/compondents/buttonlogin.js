ed3bc2a0442b4f5a8e1ca42a66f25604dfb2e3ae
Alex Shibu <alexshibu04@gmail.com>
2025-11-22 14:42:45 -0500

    sexy landing page

--- app/compondents/buttonlogin.js
+++ app/compondents/buttonlogin.js
@@ -2,9 +2,20 @@ import Link from "next/link";
 
 const ButtonLogin = (props) => {
   if (props.isLoggedIn) {
-    return <Link href="/dashboard">Dashboard</Link>;
+    return (
+      <Link href="/dashboard" className="btn btn-primary">
+        Dashboard
+      </Link>
+    );
   } else {
-    return <Link href="/login">Login</Link>;
+    return (
+      <Link
+        href="/login"
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Login
+      </Link>
+    );
   }
 };
 
--- app/page.js
+++ app/page.js
@@ -1,13 +1,32 @@
 import Image from "next/image";
 import ButtonLogin from "./compondents/buttonlogin";
+
 export default function Home() {
   const isLoggedIn = false;
+  const name = "alex";
   return (
     <main>
-      <div>
-        <h1>Hello World</h1>
-      </div>{" "}
-      <ButtonLogin isLoggedIn={isLoggedIn}>Login</ButtonLogin>{" "}
+      <div className="bg-base-200">
+        <section className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+          <div className="font-bold ">ideaBox</div>
+          <div className="space-x-4 text-red-500 max-md:hidden">
+            <a className="link link-hover"> Pricing</a>
+            <a className="link link-hover"> FAQ</a>
+          </div>
+          <ButtonLogin isLoggedIn={isLoggedIn} />
+        </section>
+      </div>
+      <section className="text-center py-32 p-8 max-w-3xl mx-auto">
+        <div>
+          <h1 className="text-4xl font-extrabold mb-6 lg:text-6xl">
+            Hello {name}
+          </h1>
+        </div>
+        <div className="mb-6 text-xl opacity-50">
+          <h2>Fish out of watter</h2>
+        </div>
+        <ButtonLogin isLoggedIn={isLoggedIn} />
+      </section>
     </main>
   );
 }
